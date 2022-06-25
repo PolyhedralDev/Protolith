0edeeb8be2f1dff81cfbfbf6a13709af36d8f84f
dfsek <dfsek@protonmail.com>
2022-06-24 21:48:01 -0700

    implement prisms

---
+++ src/main/java/com/dfsek/protolith/optics/interact/Re.java
@@ -0,0 +1,44 @@
+package com.dfsek.protolith.optics.interact;
+
+import com.dfsek.protolith.functor.Profunctor;
+import com.dfsek.protolith.functor.function.Fun;
+import com.dfsek.protolith.functor.functors.Const;
+import com.dfsek.protolith.functor.functors.Identity;
+import com.dfsek.protolith.functor.functors.Tagged;
+import com.dfsek.protolith.optics.Optic;
+import io.vavr.Function1;
+
+public final class Re<S, T, A, B> implements
+        Function1<Optic<? super Tagged<?, ?>, ? super Identity<?>, S, T, A, B>,
+                Optic<Profunctor<?, ?, ?>, Const<T, ?>, B, B, T, T>> {
+
+    private static final Re<?, ?, ?, ?> INSTANCE = new Re<>();
+
+    private Re() {
+    }
+
+    @Override
+    public Optic<Profunctor<?, ?, ?>, Const<T, ?>, B, B, T, T> apply(
+            Optic<? super Tagged<?, ?>, ? super Identity<?>, S, T, A, B> optic) {
+        return Optic.<Profunctor<?, ?, ?>, Const<T, ?>, B, B, T, T,
+                Const<T, T>, Const<T, B>,
+                Profunctor<T, Const<T, T>, ? extends Profunctor<?, ?, ?>>,
+                Profunctor<B, Const<T, B>, ? extends Profunctor<?, ?, ?>>>optic(
+                pafb -> pafb.diMap(
+                        (Function1<B, T>) b -> optic.<Tagged<?, ?>, Identity<?>, Identity<B>, Identity<T>,
+                                Tagged<A, Identity<B>>, Tagged<S, Identity<T>>>apply(new Tagged<>(new Identity<>(b))).get().get(),
+                        ttConst -> new Const<>(ttConst.get())));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <S, T, A, B> Re<S, T, A, B> re() {
+        return (Re<S, T, A, B>) INSTANCE;
+    }
+
+    public static <S, T, A, B> Optic<Profunctor<?, ?, ?>, Const<T, ?>, B, B, T, T> re(
+            Optic<? super Tagged<?, ?>, ? super Identity<?>, S, T, A, B> optic) {
+        return Re.<S, T, A, B>re().apply(optic);
+    }
+
+
+}
--- src/main/java/com/dfsek/protolith/optics/prism/Prism.java
+++ src/main/java/com/dfsek/protolith/optics/prism/Prism.java
@@ -6,15 +6,13 @@ import com.dfsek.protolith.functor.functors.Identity;
 import com.dfsek.protolith.functor.profunctors.Cocartesian;
 import com.dfsek.protolith.monad.Monad;
 import com.dfsek.protolith.optics.Optic;
+import com.dfsek.protolith.optics.interact.Matching;
 import com.dfsek.protolith.optics.interact.Re;
-import com.dfsek.protolith.optics.interact.Set;
 import com.dfsek.protolith.optics.interact.View;
-import com.dfsek.protolith.optics.lens.Lens;
 import io.vavr.Function1;
 import io.vavr.Tuple2;
 import io.vavr.control.Either;
 
-import static com.dfsek.protolith.optics.interact.View.view;
 
 @FunctionalInterface
 public interface Prism<S, T, A, B> extends
@@ -23,7 +21,12 @@ public interface Prism<S, T, A, B> extends
     static <S, T, A, B> Prism<S, T, A, B> prism(Optic<? super Cocartesian<?, ?, ?>, ? super Functor<?, ?>, S, T, A, B> optic) {
         return new Prism<>() {
             @Override
-            public <PN extends Profunctor<?, ?, ? extends Cocartesian<?, ?, ?>>, FN extends Functor<?, ? extends Functor<?, ?>>, FB extends Functor<B, ? extends FN>, FT extends Functor<T, ? extends FN>, PAFB extends Profunctor<A, FB, ? extends PN>, PSFT extends Profunctor<S, FT, ? extends PN>> PSFT apply(PAFB pafb) {
+            public <PN extends Profunctor<?, ?, ? extends Cocartesian<?, ?, ?>>,
+                    FN extends Functor<?, ? extends Functor<?, ?>>,
+                    FB extends Functor<B, ? extends FN>,
+                    FT extends Functor<T, ? extends FN>,
+                    PAFB extends Profunctor<A, FB, ? extends PN>,
+                    PSFT extends Profunctor<S, FT, ? extends PN>> PSFT apply(PAFB pafb) {
                 return optic.apply(pafb);
             }
         };
@@ -42,20 +45,28 @@ public interface Prism<S, T, A, B> extends
         ));
     }
 
-    default Tuple2<Function1<? super B, ? extends T>, Function1<? super S, ? extends Either<T, A>>> unPrism() {
-        return new Tuple2<>(Re.<S, T, A, B>re().andThen(view()), matching(), this);
+    default Function1<? super B, ? extends T> build() {
+        return b -> View.view(Re.re(this), b);
+    }
+
+    default Function1<? super S, ? extends Either<T, A>> match() {
+        return s -> Matching.matching(this).apply(s);
+    }
+
+    default Tuple2<Function1<? super B, ? extends T>, Function1<? super S, ? extends Either<T, A>>> deconstruct() {
+        return new Tuple2<>(build(), match());
     }
 
     @Override
     default <U> Prism<S, U, A, B> flatMap(Function1<? super T, ? extends Monad<U, Prism<S, ?, A, B>>> f) {
-        return unPrism()
-                .into((bt, seta) -> Prism.<S, U, A, B>prism(
-                        s -> seta.apply(s).<Either<U, A>>match(t -> matching(f.apply(t).<Prism<S, U, A, B>>coerce(), s),
+        return deconstruct()
+                .apply((bt, seta) -> Prism.<S, U, A, B>prism(
+                        s -> seta.apply(s).<Either<U, A>>fold(t -> Matching.matching(f.apply(t).<Prism<S, U, A, B>>coerce(), s),
                                 Either::right),
-                        b -> View.<B, B, U, U>view(re(f.apply(bt.apply(b)).coerce())).apply(b)));
+                        b -> View.<B, B, U, U>view(Re.re(f.apply(bt.apply(b)).coerce())).apply(b)));
     }
 
     default <U> Prism<S, U, A, B> pure(U u) {
-        return prism(Function1.constant(Either.right(u)), Function1.constant(u));
+        return prism(Function1.constant(Either.left(u)), Function1.constant(u));
     }
 }

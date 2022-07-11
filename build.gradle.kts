f526d1728c0c242bd5a4abdb7207dabbcbf09566
dfsek <dfsek@protonmail.com>
2022-07-11 06:41:39 -0700

    add Strata dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
     api("io.vavr:vavr:0.10.4")
+
+    api("ca.solo-studios:strata:1.3.0")
 }
 
 tasks.getByName<Test>("test") {
